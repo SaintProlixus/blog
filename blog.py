14f6dc4eded48031b3c1c7df49ff5c285247d717
Nate Stone <nrstone@uab.edu>
2019-08-24 23:52:50 -0500

    Add routes for login and register

--- blog.py
+++ blog.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = 'f593ed484e91e4d32756d75c2d1b4163'
 
 posts = [
     {
@@ -25,6 +28,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def register():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == '__main__':
