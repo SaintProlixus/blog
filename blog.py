431b13c55364bb9373511940102a921a05d52410
Nate Stone <nrstone@uab.edu>
2019-08-24 19:54:35 -0500

    Add some dummy post data to test play with jinja rendering

--- blog.py
+++ blog.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Nate Stone",
+        "title": "Post 1",
+        "content": "Some stuff",
+        "date_posted": "February 24, 2019"
+    },
+    {
+        "author": "Nate Stone",
+        "title": "Post 2",
+        "content": "Some more stuff",
+        "date_posted": "February 25, 2019"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
